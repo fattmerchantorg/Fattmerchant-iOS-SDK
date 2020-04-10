bbe826a5dc2cd482ec3b513f6444e9c705e98069
Tulio Troncoso <tuliot@live.com>
2020-04-10 10:27:08 -0400

    [PHX-295] Make MobileReader init public

--- fattmerchant-ios-sdk/Models/MobileReader.swift
+++ fattmerchant-ios-sdk/Models/MobileReader.swift
@@ -12,7 +12,11 @@ import Foundation
 public class MobileReader: CustomStringConvertible {
   public var name: String
 
-  init(name: String) {
+  /// Initialize a MobileReader by name
+  ///
+  /// This name must match the real name of the mobile reader, or mobile reader operations will not work
+  /// - Parameter name: The name of the mobile reader
+  public init(name: String) {
     self.name = name
   }
 
