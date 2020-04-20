31cce099096a626702ca40b3bbeafa048d4d08c4
Tulio Troncoso <tuliot@live.com>
2020-04-20 02:48:19 -0400

    [PHX-314] Add info fields to MobileReader
    Things like serialnumber and firmware version

---
+++ fattmerchant-ios-sdk/Cardpresent/Usecase/GetConnectedMobileReader.swift
@@ -0,0 +1,9 @@
+//
+//  GetConnectedMobileReader.swift
+//  fattmerchant-ios-sdk
+//
+//  Created by Tulio Troncoso on 4/20/20.
+//  Copyright © 2020 Fattmerchant. All rights reserved.
+//
+
+import Foundation
