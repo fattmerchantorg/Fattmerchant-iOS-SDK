dcc920da42a382563ef6f88db3c84828528cfdea
Tulio Troncoso <tuliot@live.com>
2020-05-18 09:58:04 -0400

    [PHX-388] Throw error if reader connection fails

--- fattmerchant-ios-sdk/Cardpresent/Usecase/ConnectMobileReader.swift
+++ fattmerchant-ios-sdk/Cardpresent/Usecase/ConnectMobileReader.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+enum ConnectMobileReaderException: OmniException {
+  static var mess: String = "Could not connect mobile reader"
+
+  case couldNotConnectMobileReader(reader: MobileReader)
+}
+
 class ConnectMobileReader {
 
   var mobileReaderDriverRepository: MobileReaderDriverRepository
@@ -18,13 +24,19 @@ class ConnectMobileReader {
     self.mobileReader = mobileReader
   }
 
-  func start(completion: @escaping (Bool) -> Void) {
+  func start(onConnected: @escaping (MobileReader) -> Void, onFailed: @escaping(OmniException) -> Void) {
 
     // First, try to see if the MobileReaderDriverRepo knows which Driver this MobileReader belongs to
     mobileReaderDriverRepository.getDriverFor(mobileReader: mobileReader) { (driver) in
 
       if let driver = driver {
-        driver.connect(reader: self.mobileReader, completion: completion)
+        driver.connect(reader: self.mobileReader, completion: { connected in
+          if connected {
+            onConnected(self.mobileReader)
+          } else {
+            onFailed(ConnectMobileReaderException.couldNotConnectMobileReader(reader: self.mobileReader))
+          }
+        })
       } else {
         // Try to connect the reader with each driver
         let drivers = self.mobileReaderDriverRepository.allDrivers()
@@ -41,8 +53,10 @@ class ConnectMobileReader {
 
             driver.connect(reader: self.mobileReader) { connected in
               if connected {
-                completion(connected)
+                onConnected(self.mobileReader)
                 callbackExecuted = true
+              } else {
+                onFailed(ConnectMobileReaderException.couldNotConnectMobileReader(reader: self.mobileReader))
               }
 
               semaphore.signal()
