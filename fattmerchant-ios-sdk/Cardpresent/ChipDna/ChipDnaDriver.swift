51a7b9bef0f186ddce6bf3c15bba288ec80e99b3
Tulio Troncoso <tuliot@live.com>
2020-06-29 04:27:37 -0400

    Fix disconnect for ChipDna reader

--- fattmerchant-ios-sdk/Cardpresent/ChipDna/ChipDnaDriver.swift
+++ fattmerchant-ios-sdk/Cardpresent/ChipDna/ChipDnaDriver.swift
@@ -155,7 +155,7 @@ class ChipDnaDriver: NSObject, MobileReaderDriver {
     ChipDnaMobile.sharedInstance()?.setProperties(requestParams)
     ChipDnaMobile.addConnectAndConfigureFinishedTarget(self, action: #selector(onConnectAndConfigure(parameters:)))
     didConnectAndConfigure = { connectedReader in
-      if connectedReader != nil, let serial = connectedReader.serialNumber {
+      if let connectedReader = connectedReader, let serial = connectedReader.serialNumber {
         self.familiarSerialNumbers.append(serial)
       }
       completion(connectedReader)
