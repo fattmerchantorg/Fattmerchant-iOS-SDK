0669d22accfe4da3b5aa27af0275a46027c155c6
Hassan Nazari <hassan.nazari@fattmerchant.com>
2020-08-07 15:27:53 -0400

    Added public initializer to CatalogItem

--- fattmerchant-ios-sdk/Models/CatalogItem.swift
+++ fattmerchant-ios-sdk/Models/CatalogItem.swift
@@ -9,14 +9,22 @@
 import Foundation
 
 public struct CatalogItem: Model, Codable {
-    /// The omni id of the item
-    public var id: String?
-    /// The name of the item
-    public var item: String
-    /// The description of the item
-    public var details: String
-    ///. The number of items to be purchased
-    public var quantity: Int
-    /// The price of the item in dollars
-    public var price: Double
+  /// The omni id of the item
+  public var id: String?
+  /// The name of the item
+  public var item: String?
+  /// The description of the item
+  public var details: String?
+  ///. The number of items to be purchased
+  public var quantity: Int
+  /// The price of the item in dollars
+  public var price: Double
+  
+  public init(id: String?, item: String?, details: String?, quantity: Int, price: Double) {
+     self.id = id
+     self.item = item
+     self.details = details
+     self.quantity = quantity
+     self.price = price
+   }
 }
