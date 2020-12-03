e5431c92be55fd9933288eff73dd0fe7da65ea4e
Tulio Troncoso <tuliot@live.com>
2020-12-03 14:31:36 -0500

    [MOB-339] Update TokenizePaymentMethod usecase

--- fattmerchant-ios-sdk/Cardpresent/Usecase/TokenizePaymentMethod.swift
+++ fattmerchant-ios-sdk/Cardpresent/Usecase/TokenizePaymentMethod.swift
@@ -14,6 +14,7 @@ enum TokenizePaymentMethodException: OmniException {
   case tokenizationError
   case couldNotParsePaymentMethodError
   case merchantMissingHostedPaymentsToken
+  case noCreditCardOrBankAccountSupplied
 }
 
 /// Tokenizes a payment method
@@ -27,38 +28,63 @@ class TokenizePaymentMethod {
   /// The Merchant that will be associated with the tokenized payment method
   var merchant: Merchant
 
+  /// The card to tokenize. If supplied, bankAccount must be nil
+  var creditCard: CreditCard?
+
+  /// The bank account to tokenize. If supplied, creditCard must be nil
+  var bankAccount: BankAccount?
+
+  var customerRepository: CustomerRepository
+  var paymentMethodRepository: PaymentMethodRepository
+
   /// Initializes a TokenizePaymentMethod instance with OmniApi and Merchant
   init(omniApi: OmniApi, merchant: Merchant) {
     self.omniApi = omniApi
     self.merchant = merchant
+    customerRepository = CustomerRepository(omniApi: omniApi)
+    paymentMethodRepository = PaymentMethodRepository(omniApi: omniApi)
   }
 
-  func start<T: Codable>(codablePaymentMethod: T, completion: @escaping (PaymentMethod) -> Void, failure: @escaping (OmniException) -> Void) {
-    guard let hostedPaymentsToken = merchant.hostedPaymentsToken else {
-      failure(Exception.merchantMissingHostedPaymentsToken)
-      return
+  func start(completion: @escaping (PaymentMethod) -> Void, failure: @escaping (OmniException) -> Void) {
+
+    guard let customerName = creditCard?.personName ?? bankAccount?.personName else {
+      return failure(CreateCustomerException.customerNameNotSupplied)
     }
-    tokenize(codablePaymentMethod, webpaymentsToken: hostedPaymentsToken, completion: completion, failure: failure)
+
+    createCustomer(customerName) { (customer, error) in
+      guard error == nil, let customer = customer else {
+        return failure(error ?? CreateCustomerException.unknown)
+      }
+
+      var paymentMethod: PaymentMethod
+
+      if let card = self.creditCard {
+        paymentMethod = PaymentMethod(card: card, customer: customer)
+      } else if let bank = self.bankAccount {
+        paymentMethod = PaymentMethod(bank: bank, customer: customer)
+      } else {
+        return failure(TokenizePaymentMethodException.noCreditCardOrBankAccountSupplied)
+      }
+
+      self.paymentMethodRepository.create(model: paymentMethod, completion: completion, error: failure)
+    }
   }
 
-  private func tokenize<T: Codable>(_ codablePaymentMethod: T, webpaymentsToken: String, completion: @escaping (PaymentMethod) -> Void, failure: @escaping (TokenizePaymentMethodException) -> Void) {
-    guard let data = encode(codablePaymentMethod: codablePaymentMethod) else {
-      failure(.couldNotParsePaymentMethodError)
-      return
-    }
-
-    let path = "/webpayment/\(webpaymentsToken)/tokenize"
-
-    omniApi.request(method: "post", urlString: path, body: data, completion: completion) { _ in
-      failure(.couldNotParsePaymentMethodError)
-    }
-
+  private func createCustomer(_ card: CreditCard, completion: @escaping (Customer?, OmniException?) -> Void) {
+    createCustomer(card.personName, completion: completion)
   }
 
-  internal func encode<T: Codable>(codablePaymentMethod: T) -> Data? {
-    let encoder = JSONEncoder()
-    encoder.keyEncodingStrategy = .convertToSnakeCase
-    return try? encoder.encode(codablePaymentMethod)
+  private func createCustomer(_ bankAccount: BankAccount, completion: @escaping (Customer?, OmniException?) -> Void) {
+    createCustomer(bankAccount.personName, completion: completion)
+  }
+
+  private func createCustomer(_ name: String, completion: @escaping (Customer?, OmniException?) -> Void) {
+    let customerToCreate = Customer(fullName: name)
+    customerRepository.create(model: customerToCreate) { (createdCustomer) in
+      completion(createdCustomer, nil)
+    } error: { (error) in
+      completion(nil, error)
+    }
   }
 
 }
