6617d97f264161b2072b81c61ae68dee096f398c
alyssa demetro <alyssa.demetro@fattmerchant.com>
2026-04-10 11:21:06 -0400

    fix(PHO-4826): apply firmware updates

--- Sources/Fattmerchant/Cardpresent/Omni/Drivers/ChipDna/ChipDnaDriver.swift
+++ Sources/Fattmerchant/Cardpresent/Omni/Drivers/ChipDna/ChipDnaDriver.swift
@@ -657,21 +657,10 @@ class ChipDnaDriver: NSObject, MobileReaderDriver, TapDriver {
 
         guard let onConnectAndConfigureCallback = onConnectAndConfigureCallback
         else { return }
-       
         if parameters[CCParamResult] != CCValueTrue {
-        // Check for error details
-        let errorCodes = parameters[CCParamErrors] ?? "Unknown"
-        let errorDescription = parameters[CCParamErrorDescription] ?? "No description"
-        let CCParamResult = parameters[CCParamResult] ?? "no ccparamresult"
-        
-        print("❌ Connection failed")
-        print("Error codes: \(errorCodes)")
-        print("Error description: \(errorDescription)")
-        print("CCParamResult: \(CCParamResult)")
-        
-        onConnectAndConfigureCallback(nil)
-        return
-    }
+            onConnectAndConfigureCallback(nil)
+            return
+        }
 
         // Figure out the reader details and pass them along
         onConnectAndConfigureCallback(ChipDnaDriver.getConnectedReader())
