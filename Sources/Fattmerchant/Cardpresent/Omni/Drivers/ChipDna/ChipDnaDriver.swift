77b8f915e9721d0f4f2ffd757bddf1620c6257f6
alyssa demetro <alyssa.demetro@fattmerchant.com>
2026-05-08 12:19:31 -0400

    feat(PHO-3834): force tms update for bbpos

--- Sources/Fattmerchant/Cardpresent/Omni/Drivers/ChipDna/ChipDnaDriver.swift
+++ Sources/Fattmerchant/Cardpresent/Omni/Drivers/ChipDna/ChipDnaDriver.swift
@@ -201,6 +201,8 @@ class ChipDnaDriver: NSObject, MobileReaderDriver, TapDriver {
 
         if reader.name.uppercased().hasPrefix("IDTECH") {
             requestParams.setValue(CCValueTrue, forKey: CCParamApplyFirmwareUpdate)
+        } else {
+            requestParams.setValue(CCValueTrue, forKey: CCParamForceTmsUpdate)
         }
 
         ChipDnaMobile.sharedInstance()?.setProperties(requestParams)
