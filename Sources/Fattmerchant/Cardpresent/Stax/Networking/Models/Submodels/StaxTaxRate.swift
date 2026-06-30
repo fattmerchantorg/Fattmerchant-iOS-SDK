2b707b4e2b0c77ee42e5682e5510ffc748288ade
alyssa demetro <alyssa.demetro@fattmerchant.com>
2026-06-30 14:26:55 -0400

    fix(test-rate-fix): test

--- Sources/Fattmerchant/Cardpresent/Stax/Networking/Models/Submodels/StaxTaxRate.swift
+++ Sources/Fattmerchant/Cardpresent/Stax/Networking/Models/Submodels/StaxTaxRate.swift
@@ -3,9 +3,24 @@ struct StaxTaxRate: Codable {
   let name: String?
   let rate: Double?
   let isDefault: Bool?
-  
+
   enum CodingKeys: String, CodingKey {
     case id, name, rate
     case isDefault = "is_default"
+  }
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    id = try container.decodeIfPresent(String.self, forKey: .id)
+    name = try container.decodeIfPresent(String.self, forKey: .name)
+    isDefault = try container.decodeIfPresent(Bool.self, forKey: .isDefault)
+
+    if let doubleRate = try? container.decodeIfPresent(Double.self, forKey: .rate) {
+      rate = doubleRate
+    } else if let stringRate = try? container.decodeIfPresent(String.self, forKey: .rate) {
+      rate = Double(stringRate)
+    } else {
+      rate = nil
+    }
   }
 }
